aafc11a92e7af4354b165712eb60fcc302aba064
somdatta <nag.somdatta@gmail.com>
2015-01-27 14:28:03 +0530

    MyApplication2

---
+++ MyApp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const string message = "Euraqua!!!!";
+
+            const string caption = "Hello";
+            var result = MessageBox.Show(message, caption,
+                                         MessageBoxButtons.OK,
+                                         MessageBoxIcon.Information); 
+        }
+    }
+}
